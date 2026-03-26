4cc2848d21169d1702706cfa9d166033915d2b45
sribalaji <sribalajimuruganandham@gmail.com>
2026-03-26 18:34:08 +0530

    jest unit testing added

---
+++ index.test.js
@@ -0,0 +1,12 @@
+// app.test.js
+const request = require('supertest');
+const app = require('./app');
+
+describe('GET /', () => {
+  it('should return CI', async () => {
+    const res = await request(app).get('/');
+
+    expect(res.statusCode).toBe(200);
+    expect(res.body).toEqual({ message: 'CI' });
+  });
+});
