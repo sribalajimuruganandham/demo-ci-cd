cc1fa3258ca334b08eb927234a78337e216b48de
sribalaji <sribalajimuruganandham@gmail.com>
2026-03-26 17:25:25 +0530

    4

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("CI/CD Pipeline");
+  res.send("CI/CD ");
 });
 
 app.listen(3000, () => console.log("Server running"));
