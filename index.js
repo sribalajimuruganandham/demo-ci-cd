a705b369c69aada5a6fbae8addb2681560304fa8
sribalaji <sribalajimuruganandham@gmail.com>
2026-03-26 17:50:39 +0530

    7

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("CI/CD ");
+  res.send("CI");
 });
 
 app.listen(3000, () => console.log("Server running"));
